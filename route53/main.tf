b3c43d8dd14f766b3deb2b475624968e3ec1f9eb
Andrew Jarombek <ajarombek95@gmail.com>
2019-01-22 21:30:41 -0500

    More S3 Global work

--- route53/main.tf
+++ route53/main.tf
@@ -35,3 +35,28 @@ resource "aws_route53_record" "jarombek-io-ns" {
     "${aws_route53_zone.jarombek-io.name_servers.3}"
   ]
 }
+
+resource "aws_route53_record" "global-jarombek-io-a" {
+  name = "global.jarombek.io."
+  type = "A"
+  zone_id = "${aws_route53_zone.jarombek-io.zone_id}"
+
+  # TTL for all alias records is 60 seconds
+  alias {
+    evaluate_target_health = false
+    name = ""
+    zone_id = ""
+  }
+}
+
+resource "aws_route53_record" "www-global-jarombek-io-a" {
+  name = "www.global.jarombek.io."
+  type = "A"
+  zone_id = "${aws_route53_zone.jarombek-io.zone_id}"
+
+  alias {
+    evaluate_target_health = false
+    name = ""
+    zone_id = ""
+  }
+}
--- s3/main.tf
+++ s3/main.tf
@@ -63,3 +63,52 @@ resource "aws_s3_bucket" "www-global-jarombek-io" {
     redirect_all_requests_to = "https://global.jenkins.io"
   }
 }
+
+resource "aws_s3_bucket_object" "index-json" {
+  bucket = "${aws_s3_bucket.global-jarombek-io.id}"
+  key = "index.json"
+  source = "global/index.json"
+  etag = "${md5(file("global/index.json"))}"
+}
+
+resource "aws_s3_bucket_object" "fonts-css" {
+  bucket = "${aws_s3_bucket.global-jarombek-io.id}"
+  key = "fonts.css"
+  source = "global/fonts.css"
+  etag = "${md5(file("global/fonts.css"))}"
+}
+
+resource "aws_s3_bucket_object" "dyslexie-bold-ttf" {
+  bucket = "${aws_s3_bucket.global-jarombek-io.id}"
+  key = "fonts/dyslexie-bold.ttf"
+  source = "global/fonts/dyslexie-bold.ttf"
+  etag = "${md5(file("global/fonts/dyslexie-bold.ttf"))}"
+}
+
+resource "aws_s3_bucket_object" "fantasque-sans-mono-bold-ttf" {
+  bucket = "${aws_s3_bucket.global-jarombek-io.id}"
+  key = "fonts/FantasqueSansMono-Bold.ttf"
+  source = "global/fonts/FantasqueSansMono-Bold.ttf"
+  etag = "${md5(file("global/fonts/FantasqueSansMono-Bold.ttf"))}"
+}
+
+resource "aws_s3_bucket_object" "longway-regular-otf" {
+  bucket = "${aws_s3_bucket.global-jarombek-io.id}"
+  key = "fonts/Longway-Regular.otf"
+  source = "global/fonts/Longway-Regular.otf"
+  etag = "${md5(file("global/fonts/Longway-Regular.otf"))}"
+}
+
+resource "aws_s3_bucket_object" "sylexiad-sans-thin-ttf" {
+  bucket = "${aws_s3_bucket.global-jarombek-io.id}"
+  key = "fonts/SylexiadSansThin.ttf"
+  source = "global/fonts/SylexiadSansThin.ttf"
+  etag = "${md5(file("global/fonts/SylexiadSansThin.ttf"))}"
+}
+
+resource "aws_s3_bucket_object" "sylexiad-sans-thin-bold-ttf" {
+  bucket = "${aws_s3_bucket.global-jarombek-io.id}"
+  key = "fonts/SylexiadSansThin-Bold.ttf"
+  source = "global/fonts/SylexiadSansThin-Bold.ttf"
+  etag = "${md5(file("global/fonts/SylexiadSansThin-Bold.ttf"))}"
+}
