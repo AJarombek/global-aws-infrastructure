3bdd62444c75fd28e3326b6054741ebc58c0d3df
Andrew Jarombek <ajarombek95@gmail.com>
2020-06-04 18:14:31 -0400

    begin ECR and ECS jenkins (will replace EC2 jenkins)

---
+++ ecr/main.tf
@@ -0,0 +1,40 @@
+/**
+ * AWS Infrastructure for my Elastic Container Registry.
+ * Author: Andrew Jarombek
+ * Date: 6/4/2020
+ */
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+terraform {
+  required_version = ">= 0.12"
+
+  backend "s3" {
+    bucket = "andrew-jarombek-terraform-state"
+    encrypt = true
+    key = "global-aws-infrastructure/ecr"
+    region = "us-east-1"
+  }
+}
+
+resource "aws_ecr_repository" "andrew-jarombek-repository" {
+  name = "andrew-jarombek"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  tags = {
+    Name = "andrew-jarombek-container-repository"
+    Application = "all"
+    Environment = "all"
+  }
+}
+
+resource "aws_ecr_lifecycle_policy" "andrew-jarombek-repository-policy" {
+  repository = aws_ecr_repository.andrew-jarombek-repository.name
+  policy = file("repo-policy.json")
+}
