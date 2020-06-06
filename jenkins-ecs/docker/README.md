074319bb2f90c5a7087e169253af913e08e09c2d
Andrew Jarombek <ajarombek95@gmail.com>
2020-06-06 18:47:37 -0400

    jenkins almost set up and working

--- jenkins-ecs/docker/README.md
+++ jenkins-ecs/docker/README.md
@@ -4,13 +4,20 @@ Docker container for a Jenkins server.
 
 ### Commands
 
-**Run the Jenkins container locally.**
+**Start the Jenkins container locally**
 
 ```bash
+python3.8 -m venv env
+source ./env/bin/activate
+python3.8 -m pip install -r requirements.txt
+
+python3.8 push-image.py
+
+deactivate
+
 docker container stop jenkins-jarombek-io
 docker container rm jenkins-jarombek-io
 
-docker image build -t jenkins-jarombek-io:latest .
 docker container run --name jenkins-jarombek-io -p 8080:8080 -p 50000:50000 jenkins-jarombek-io:latest
 ```
 
--- jenkins-ecs/docker/push-image.py
+++ jenkins-ecs/docker/push-image.py
@@ -5,23 +5,48 @@ Date: 6/6/2020
 """
 
 import json
+import subprocess
 
 import boto3
-from boto3_type_annotations.secretsmanager import Client
+from boto3_type_annotations.secretsmanager import Client as SecretsManagerClient
+from boto3_type_annotations.sts import Client as STSClient
 
 
 def main():
-    secretsmanager: Client = boto3.client('secretsmanager')
+    private_key = get_github_key()
+    account_id = get_account_id()
+
+    with open("jenkins-template.yaml", "r+") as file:
+        jenkins_config: str = file.read()
+
+    jenkins_config = jenkins_config.replace("${SSH_PRIVATE_KEY}", private_key)
+
+    with open("jenkins.yaml", "w") as file:
+        file.write(jenkins_config)
+
+    subprocess.call(["./prepare-image.sh", account_id])
+
+
+def get_github_key():
+    """
+    Get my GitHub accounts private key from AWS Secrets Manager.
+    :return: The private key.
+    """
+    secretsmanager: SecretsManagerClient = boto3.client('secretsmanager')
     secret = secretsmanager.get_secret_value(SecretId=f"github-secret")
 
     secret_string = secret.get('SecretString')
     secret_dict: dict = json.loads(secret_string)
-    private_key = secret_dict["private_key"]
+    return secret_dict["private_key"]
 
-    with open("jenkins-template.yaml", "r+") as fp:
-        jenkins_config: str = fp.read()
 
-    jenkins_config.replace()
+def get_account_id():
+    """
+    Get the current users AWS account ID.
+    :return: The account ID.
+    """
+    sts: STSClient = boto3.client('sts')
+    return sts.get_caller_identity().get('Account')
 
 
 if __name__ == '__main__':
