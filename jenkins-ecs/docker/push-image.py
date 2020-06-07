80a25e51357a00f50e16871b954d85162040ed6e
Andrew Jarombek <ajarombek95@gmail.com>
2020-06-07 18:53:05 -0400

    emails sent from the jenkins server with gmail

--- jenkins-ecs/docker/push-image.py
+++ jenkins-ecs/docker/push-image.py
@@ -16,8 +16,12 @@ def main():
     """
     Build a Jenkins image with secrets from AWS.
     """
-    private_key = get_github_key()
+    secretsmanager: SecretsManagerClient = boto3.client('secretsmanager')
+
+    private_key = get_github_key(secretsmanager)
     account_id = get_account_id()
+    jenkins_password = get_jenkins_password(secretsmanager)
+    google_account_password = get_google_account_password(secretsmanager)
 
     with open("jenkins-template.yaml", "r+") as file:
         jenkins_config: str = file.read()
@@ -30,6 +34,8 @@ def main():
     formatted_private_key += "\""
 
     jenkins_config = jenkins_config.replace("${SSH_PRIVATE_KEY}", formatted_private_key)
+    jenkins_config = jenkins_config.replace("${JENKINS_PASSWORD}", jenkins_password)
+    jenkins_config = jenkins_config.replace("${GOOGLE_ACCOUNT_PASSWORD}", google_account_password)
 
     with open("jenkins.yaml", "w") as file:
         file.write(jenkins_config)
@@ -37,17 +43,43 @@ def main():
     subprocess.call(["./prepare-image.sh", account_id])
 
 
-def get_github_key() -> str:
+def get_github_key(secretsmanager: SecretsManagerClient) -> str:
     """
     Get my GitHub accounts private key from AWS Secrets Manager.
+    :param secretsmanager: Boto3 Secrets Manager client used to get secrets.
     :return: The private key.
     """
-    secretsmanager: SecretsManagerClient = boto3.client('secretsmanager')
     secret = secretsmanager.get_secret_value(SecretId=f"github-secret")
 
     secret_string = secret.get('SecretString')
     secret_dict: dict = json.loads(secret_string)
     return secret_dict["private_key"]
+
+
+def get_jenkins_password(secretsmanager: SecretsManagerClient) -> str:
+    """
+    Get the password for jenkins.jarombek.io from AWS Secrets Manager.
+    :param secretsmanager: Boto3 Secrets Manager client used to get secrets.
+    :return: The Jenkins password.
+    """
+    secret = secretsmanager.get_secret_value(SecretId="jenkins-secret")
+
+    secret_string = secret.get('SecretString')
+    secret_dict: dict = json.loads(secret_string)
+    return secret_dict["password"]
+
+
+def get_google_account_password(secretsmanager: SecretsManagerClient) -> str:
+    """
+    Get a Google account password from AWS Secrets Manager.
+    :param secretsmanager: Boto3 Secrets Manager client used to get secrets.
+    :return: The Google account password.
+    """
+    secret = secretsmanager.get_secret_value(SecretId="google-account-secret")
+
+    secret_string = secret.get('SecretString')
+    secret_dict: dict = json.loads(secret_string)
+    return secret_dict["password"]
 
 
 def get_account_id() -> str:
