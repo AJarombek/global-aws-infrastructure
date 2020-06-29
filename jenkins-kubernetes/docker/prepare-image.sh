3b9a898a8efe7025dd239dc3989b11b388d6dd88
Andrew Jarombek <ajarombek95@gmail.com>
2020-06-28 21:52:17 -0400

    jenkins yaml changes, additional pipeline permissions

--- jenkins-kubernetes/docker/prepare-image.sh
+++ jenkins-kubernetes/docker/prepare-image.sh
@@ -7,6 +7,6 @@
 ACCOUNT_ID=$1
 IMAGE_TAG=$2
 
-sudo docker image build -t jenkins-jarombek-io:latest .
+sudo docker image build -t jenkins-jarombek-io:latest --network=host .
 sudo docker image tag jenkins-jarombek-io:latest \
     ${ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com/jenkins-jarombek-io:${IMAGE_TAG}
