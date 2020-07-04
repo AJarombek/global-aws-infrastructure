a2ce84afe0219b8484047016d7218ab0ff3b75b9
Andrew Jarombek <ajarombek95@gmail.com>
2020-07-03 20:35:23 -0400

    complete jenkins kubernetes aws tests

--- test/suites/testJenkinsKubernetes.py
+++ test/suites/testJenkinsKubernetes.py
@@ -6,11 +6,20 @@ Date: 4/27/2019
 
 import unittest
 import boto3
+import os
 from boto3_type_annotations.route53 import Client as Route53Client
 from boto3_type_annotations.acm import Client as ACMClient
 from boto3_type_annotations.ecr import Client as ECRClient
 
 from utils.route53 import Route53
+from utils.securityGroup import SecurityGroup
+
+try:
+    env = os.environ['TEST_ENV']
+    prod_env = env == "prod"
+except KeyError:
+    env = "prod"
+    prod_env = True
 
 
 class TestJenkinsKubernetes(unittest.TestCase):
@@ -22,6 +31,40 @@ class TestJenkinsKubernetes(unittest.TestCase):
         self.route53: Route53Client = boto3.client('route53')
         self.acm: ACMClient = boto3.client('acm')
         self.ecr: ECRClient = boto3.client('ecr')
+        self.prod_env = prod_env
+
+    def test_acm_wildcard_cert_issued(self) -> None:
+        """
+        Test that the '*.jenkins.jarombek.io' wildcard ACM certificate exists
+        """
+        acm_certificates = self.acm.list_certificates(CertificateStatuses=['ISSUED'])
+        for cert in acm_certificates.get('CertificateSummaryList'):
+            if cert.get('DomainName') == '*.jenkins.jarombek.io':
+                self.assertTrue(True)
+                return
+
+        self.assertTrue(False)
+
+    def test_acm_dev_wildcard_cert_issued(self) -> None:
+        """
+        Test that the '*.dev.jenkins.jarombek.io' wildcard ACM certificate exists
+        """
+        acm_certificates = self.acm.list_certificates(CertificateStatuses=['ISSUED'])
+        for cert in acm_certificates.get('CertificateSummaryList'):
+            if cert.get('DomainName') == '*.dev.jenkins.jarombek.io':
+                self.assertTrue(True)
+                return
+
+        self.assertTrue(False)
+
+    def test_ecr_repository_exists(self) -> None:
+        """
+        Test that the 'jenkins-jarombek-io' ECR repository exists.
+        """
+        repos = self.ecr.describe_repositories(repositoryNames=['jenkins-jarombek-io']).get('repositories')
+        self.assertEqual(1, len(repos))
+        self.assertEqual('jenkins-jarombek-io', repos[0].get('repositoryName'))
+        self.assertEqual('MUTABLE', repos[0].get('imageTagMutability'))
 
     def test_jenkins_jarombek_io_a_record_exists(self) -> None:
         """
@@ -34,3 +77,34 @@ class TestJenkinsKubernetes(unittest.TestCase):
             return
 
         self.assertTrue(a_record.get('Name') == 'jenkins.jarombek.io.' and a_record.get('Type') == 'A')
+
+    @unittest.skipIf(not prod_env, 'Jenkins server not setup in development.')
+    def test_jenkins_load_balancer_security_group(self) -> None:
+        sg = SecurityGroup.get_security_groups(name=f'jenkins-{env}-lb-security-group')[0]
+
+        self.assertTrue(all([
+            sg.get('GroupName') == f'jenkins-{env}-lb-security-group',
+            TestJenkinsKubernetes.validate_jenkins_load_balancer_sg_rules(
+                sg.get('IpPermissions'), sg.get('IpPermissionsEgress')
+            )
+        ]))
+
+    @staticmethod
+    def validate_jenkins_load_balancer_sg_rules(ingress: list, egress: list):
+        """
+        Ensure that the jenkins-{env}-lb-security-group security group rules are as expected
+        :param ingress: Ingress rules for the security group
+        :param egress: Egress rules for the security group
+        :return: True if the security group rules exist as expected, False otherwise
+        """
+        ingress_80 = SecurityGroup.validate_sg_rule_cidr(ingress[0], 'tcp', 80, 80, '0.0.0.0/0')
+        ingress_443 = SecurityGroup.validate_sg_rule_cidr(ingress[1], 'tcp', 443, 443, '0.0.0.0/0')
+        egress_neg1 = SecurityGroup.validate_sg_rule_cidr(egress[0], '-1', 0, 0, '0.0.0.0/0')
+
+        return all([
+            len(ingress) == 2,
+            ingress_80,
+            ingress_443,
+            len(egress) == 1,
+            egress_neg1
+        ])
