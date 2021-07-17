61e07012be3f140daf18d33b3be19c80147f12f2
ajarombek <ajarombek95@gmail.com>
2021-07-17 16:31:47 -0400

    daily cost lambda infrastructure

---
+++ lambda/daily-cost/lambda.py
@@ -0,0 +1,11 @@
+"""
+Lambda function used to determine the daily cost of AWS expenses.
+Author: Andrew Jarombek
+Date: 7/17/2021
+"""
+
+import boto3
+
+
+def handler(event, context):
+    pass
---
+++ lambda/main.tf
@@ -0,0 +1,92 @@
+/**
+ * AWS Lambda functions definitions and configuration.
+ * Author: Andrew Jarombek
+ * Date: 7/15/2021
+ */
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+terraform {
+  required_version = ">= 1.0.1"
+
+  required_providers {
+    aws = ">= 3.48.0"
+  }
+
+  backend "s3" {
+    bucket  = "andrew-jarombek-terraform-state"
+    encrypt = true
+    key     = "global-aws-infrastructure/lambda"
+    region  = "us-east-1"
+  }
+}
+
+resource "aws_lambda_function" "daily-cost" {
+  function_name = "AWSDailyCost"
+  filename = "${path.module}/AWSDailyCost.zip"
+  description = "AWS daily cost function"
+  handler = "lambda.handler"
+  role = aws_iam_role.daily-cost-lambda-role.arn
+  runtime = "python3.8"
+  source_code_hash = filebase64sha256("${path.module}/AWSDailyCost.zip")
+  timeout = 10
+  memory_size = 128
+
+  tags = {
+    Name = "aws-daily-cost"
+    Environment = "all"
+    Application = "all"
+  }
+}
+
+resource "aws_cloudwatch_log_group" "daily-cost-log-group" {
+  name = "/aws/lambda/AWSDailyCost"
+  retention_in_days = 7
+}
+
+data "aws_iam_policy_document" "daily-cost-lambda-assume-role-policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    effect = "Allow"
+
+    principals {
+      identifiers = ["lambda.amazonaws.com"]
+      type = "Service"
+    }
+  }
+}
+
+data "aws_iam_policy_document" "daily-cost-lambda-policy" {
+  statement {
+    sid = "AWSDailyCostLambda"
+    effect = "Allow"
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+      "logs:DescribeLogStreams"
+    ]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_role" "daily-cost-lambda-role" {
+  name = "daily-cost-lambda-role"
+  path = "/global/"
+  assume_role_policy = data.aws_iam_policy_document.daily-cost-lambda-assume-role-policy.json
+  description = "IAM role for an AWS Lambda function which computes daily AWS costs"
+}
+
+resource "aws_iam_policy" "daily-cost-lambda-policy" {
+  name = "daily-cost-lambda-policy"
+  path = "/global/"
+  policy = data.aws_iam_policy_document.daily-cost-lambda-policy.json
+  description = "IAM policy for an AWS Lambda function which computes daily AWS costs"
+}
+
+resource "aws_iam_role_policy_attachment" "lambda-logging-policy-attachment" {
+  role = aws_iam_role.daily-cost-lambda-role.name
+  policy_arn = aws_iam_policy.daily-cost-lambda-policy.arn
+}
