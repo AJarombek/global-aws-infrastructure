029d230fc85953a9a164c626d637464f80901b59
ajarombek <ajarombek95@gmail.com>
2023-10-09 20:47:00 -0400

    fmt

--- test/runner.py
+++ test/runner.py
@@ -8,14 +8,12 @@ import unittest
 import sys
 
 if __name__ == "__main__":
-
     # Create the test suite
     tests = unittest.TestLoader().discover("suites")
 
     if len(sys.argv) > 1:
         log_filename = sys.argv[1]
         with open(log_filename, "w+") as log_file:
-
             # Create a test runner an execute the test suite
             runner = unittest.TextTestRunner(log_file, verbosity=3)
             result: unittest.TestResult = runner.run(tests)
